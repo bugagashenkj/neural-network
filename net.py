9a96cfee4eab89413bf90c8c35a544274a4d35f8
bugagashenkj <bugagashenkj@gmail.com>
2018-10-02 00:28:55 +0300

    Refactoring

--- net.py
+++ net.py
@@ -33,10 +33,10 @@ class Net:
             weight_deltas = [error * output * (1 - output) for error, output in zip(errors, layer_output)]
             layer_output = outputs.pop()
             errors = [0] * len(layer_output)
-            for parent_num, weight_delta in enumerate(weight_deltas):
+            for parent, weight_delta in zip(layer_weights, weight_deltas):
                 for child_num, output_data in enumerate(layer_output):
-                    layer_weights[parent_num][child_num] -= output_data * weight_delta * learning_rate
-                    errors[child_num] += layer_weights[parent_num][child_num] * weight_delta
+                    parent[child_num] -= weight_delta * output_data * learning_rate
+                    errors[child_num] += weight_delta * parent[child_num] 
             
 
     def train(self, datasets, iterations, learning_rate):
