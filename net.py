9e8d12705960de5a97e16ab32e9b3ca33cc99c53
bugagashenkj <bugagashenkj@gmail.com>
2018-10-04 11:55:56 +0300

    Refactoring

--- net.py
+++ net.py
@@ -7,38 +7,42 @@ def random_matrix(diff, a, b):
 def next_round(weight_layer, layer):
     sigmoid = lambda x: 1 / (1 + math.exp(-x))
     array_dot = lambda a, b: [a1 * b1 for a1, b1 in zip(a, b)]
+
     inputs = [sum(array_dot(weights, layer)) for weights in weight_layer]
-    layer_data = [sigmoid(input_data) for input_data in inputs]
-    return layer_data 
+    outputs = [sigmoid(input_data) for input_data in inputs]
+    return outputs 
 
 class Net:
     def __init__(self, network_struct):
         self.weights = []
         prev_layer_size = network_struct.pop(0) 
         for layer_size in network_struct:
-            weight_layer = random_matrix(0.5, layer_size, prev_layer_size)
+            weight_layer = random_matrix(0.5, prev_layer_size, layer_size)
             self.weights.append(weight_layer)
             prev_layer_size = layer_size
     
     def predict(self, layer):
-        for weight_layer in reversed(self.weights):
+        for weight_layer in self.weights:
             layer = next_round(weight_layer, layer)
-        activation_func = lambda x: round(x)
-        return list(map(activation_func, layer))
+        return [round(output) for output in layer]
 
     def train(self, datasets, iterations, learning_rate):
         for iteration in range(iterations):
             for input_data, expected_data in datasets:
-                self.train_round(input_data, expected_data, learning_rate)
+                outputs = self.train_predict(input_data)
+                self.correct_weights(outputs, expected_data, learning_rate)
     
-    def train_round(self, layer, expected_data, learning_rate):
-        outputs = []
-        for weight_layer in reversed(self.weights):
+    def train_predict(self, layer):
+        outputs = [layer]
+        for weight_layer in self.weights:
+            layer = next_round(weight_layer, layer)
             outputs.append(layer)
-            layer = next_round(weight_layer, layer) 
-        
+        return outputs
+
+    def correct_weights(self, outputs, expected_data, learning_rate):
+        layer = outputs.pop()
         errors = [output - expected for output, expected in zip(layer, expected_data)]
-        for layer_weights in self.weights:
+        for layer_weights in reversed(self.weights):
             weight_deltas = [error * output * (1 - output) for error, output in zip(errors, layer)]
             layer = outputs.pop()
             errors = [0] * len(layer)
